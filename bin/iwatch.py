ec972881c957ccf4135ecf8a32c7943f6ea7622e
Radu Ciorba <radu@devrandom.ro>
2017-01-10 17:27:30 +0200

    make iwatch script 2/3 fiendly

--- bin/iwatch.py
+++ bin/iwatch.py
@@ -1,19 +1,25 @@
 #!python
+from __future__ import print_function
 import sys
 from os import path
 
 from inotify import adapters, constants
+import six
 
 
 def wait_for_dir(watched_path):
+    watched_path = six.binary_type(watched_path.encode('utf-8'))
     i = adapters.InotifyTree(watched_path, mask=constants.IN_CLOSE_WRITE)
     for event in i.event_gen():
         if event is not None:
             filename = path.basename(event[3])
-            if filename.startswith('.#') or filename.startswith('flycheck') or filename.endswith("~"):
+            dir_name = event[2]
+            if filename.startswith(b'.#') or filename.startswith(b'flycheck') or filename.endswith(b"~"):
+                continue
+            elif dir_name.endswith(b'.cache/v/cache'):
                 continue
             else:
-                print event
+                print(event)
                 sys.exit(0)
 
 
