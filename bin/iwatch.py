b9bde54a65a071aa1ba20cce7c05559221018c1c
Radu Ciorba <rciorba@bitdefender.com>
2019-02-01 00:20:36 +0200

    update iwatch.py for python3
    Can't remember the actual issue, but it seems to do with
    inotify's handling of strings in python3

--- bin/iwatch.py
+++ bin/iwatch.py
@@ -1,4 +1,5 @@
-#!python
+#!/usr/bin/env python3
+
 from __future__ import print_function
 import sys
 from os import path
@@ -8,15 +9,15 @@ import six
 
 
 def wait_for_dir(watched_path):
-    watched_path = six.binary_type(watched_path.encode('utf-8'))
+    # watched_path = six.binary_type(watched_path.encode('utf-8'))
     i = adapters.InotifyTree(watched_path, mask=constants.IN_CLOSE_WRITE)
     for event in i.event_gen():
         if event is not None:
             filename = path.basename(event[3])
             dir_name = event[2]
-            if filename.startswith(b'.#') or filename.startswith(b'flycheck') or filename.endswith(b"~"):
+            if filename.startswith('.#') or filename.startswith('flycheck') or filename.endswith("~"):
                 continue
-            elif dir_name.endswith(b'.cache/v/cache'):
+            elif dir_name.endswith('.cache/v/cache'):
                 continue
             else:
                 print(event)
