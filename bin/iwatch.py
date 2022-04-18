2bb7febed36d9750b199d0f711dd5dfbe8b8a32c
Radu Ciorba <radu.ciorba@hotjar.com>
2022-04-18 21:23:45 +0200

    expand list of ignored patterns for iwatch

--- bin/iwatch.py
+++ bin/iwatch.py
@@ -15,14 +15,20 @@ def wait_for_dir(watched_path):
         if event is not None:
             filename = path.basename(event[3])
             dir_name = event[2]
-            if filename.startswith('.#') or filename.startswith('flycheck') or filename.endswith("~"):
-                continue
-            elif dir_name.endswith('.cache/v/cache'):
+            if any(
+                [
+                    filename.startswith(".#"),
+                    filename.startswith("flycheck"),
+                    filename.endswith("~"),
+                    filename.startswith("./.mypy_cache/"),
+                    dir_name.endswith(".cache/v/cache"),
+                ]
+            ):
                 continue
             else:
                 print(event)
                 sys.exit(0)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     wait_for_dir(sys.argv[1])
