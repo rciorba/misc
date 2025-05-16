72848af87fa4046c3a0c6345c8a9a0bd1d862341
Radu Ciorba <radu.ciorba@hotjar.com>
2025-05-16 23:13:36 +0200

    update fish helpers

--- fish/loop.fish
+++ fish/loop.fish
@@ -1,11 +1,11 @@
 function loop --description "loop"
     while python3 ~/repos/misc/bin/iwatch.py  .
-        clear
+        # clear
         $argv
         if test $status -eq 0
-             notify-send "ok" -i ~/Pictures/passing.svg -t 1000 
-         else 
-            notify-send "failed" -i ~/Pictures/failing.svg 
+             notify-send "ok" -i ~/Pictures/passing.svg -t 2000
+         else
+            notify-send "failed" -i ~/Pictures/failing.svg
         end
     end
 end
