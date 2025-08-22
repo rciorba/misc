d835f2765b4fed46614187d4cfe97370b2265a04
Radu Ciorba <rciorba@users.noreply.github.com>
2025-08-22 08:47:56 +0200

    Update search.fish

--- fish/search.fish
+++ fish/search.fish
@@ -3,15 +3,11 @@ function feo --description "fuzzy search emacs open"
          ag $argv | fzf --bind "enter:execute(eo {})" --exact --bind ctrl-k:kill-line
 end
 
-function fco --description "fuzzy search emacs open"
+function fco --description "fuzzy search vscode open"
          ag $argv | fzf --bind "enter:execute(echo {} | grep -o -E '.*:[0-9]+' | xargs code -r -g)" --exact --bind ctrl-k:kill-line
 end
 
 
 function fnd --description "fuzzy find emacs open"
          find -iname $argv | fzf --bind "enter:execute(eo {})" --exact --bind ctrl-k:kill-line
-end
-
-function far --description "fuzzy search & replace"
-         ag $argv | fzf --bind "enter:execute(eo {})" --exact --bind ctrl-k:kill-line
 end
